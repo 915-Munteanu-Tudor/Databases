da7bca3cfedbf807722a3083f3b94e33db7c062e
915-Munteanu-Tudor <72068100+915-Munteanu-Tudor@users.noreply.github.com>
2021-11-16 17:30:57 +0200

    up

--- lab1-SERVICE AUTO/Service Auto/Service Auto/Procedures.sql
+++ lab1-SERVICE AUTO/Service Auto/Service Auto/Procedures.sql
@@ -53,7 +53,7 @@ go
 go
 
 select * from Workshop
-exec DefaultWorkshopSpace12
+exec DefaultWorkshopSpace15
 exec RemoveDefault
 
 --d) add/remove a primary key
@@ -61,7 +61,7 @@ exec RemoveDefault
  create procedure AddCarOwnersPK
  as
 	alter table CarOwners
-		drop constraint IdPK
+		drop constraint IDPK
 	alter table CarOwners
 		add constraint id_name_pk primary key(ID, name)
 go
@@ -71,7 +71,7 @@ go
 	alter table CarOwners
 		drop constraint id_name_pk
 	alter table CarOwners
-		add constraint id_mechanic primary key(ID)
+		add constraint IDPK primary key(ID)
 go
 
 exec AddCarOwnersPK
@@ -117,7 +117,7 @@ create procedure CreateTableCarOwners
 as
 	create table CarOwners(
 		ID int not null
-		constraint IdPK primary key,
+		constraint IDPK primary key,
 		name varchar(35) not null,
 		car_id int not null
 	);
@@ -131,3 +131,104 @@ go
 exec CreateTableCarOwners
 select * from CarOwners
 exec DropTableCarOwners
+
+
+--version history
+
+create table VersionHistory(
+	version int
+)
+
+insert into VersionHistory values(0)
+drop table VersionHistory
+
+Create table ProcedureTable (
+	UndoProcedure varchar(100),
+	RedoProcedure varchar(100),
+	Version int primary key
+)
+
+drop table VersionHistory
+drop table ProcedureTable
+exec GoToVersion 0
+
+CREATE PROCEDURE GoToVersion (@Version int)
+as
+declare @var int;
+	set @var = (select top 1 VH.version
+				from VersionHistory VH)
+
+declare @statements char(100);
+declare @procedure nvarchar(100);
+declare @var2 int
+
+while @var != @Version
+begin
+	if @var > @Version
+		begin
+			declare UndoCursor cursor
+			for select PT.UndoProcedure
+				from ProcedureTable PT
+			open UndoCursor
+			select @var2 = 0
+			while @var2 != @var
+				begin
+					fetch from UndoCursor into @statements
+					select @var2 = @var2 + 1
+				end
+
+				select @procedure = 'exec ' + @statements
+				print @procedure
+				print 'this was the procedure'
+				exec sp_executesql @procedure
+				update VersionHistory
+					set version = version - 1
+				set @var = @var -1
+				--fetch from UndoCursor into @statements
+
+			close UndoCursor
+			deallocate UndoCursor
+		end
+			else
+				begin
+					declare RedoCursor cursor for select PT.RedoProcedure from ProcedureTable PT
+					open RedoCursor
+					select @var2 = -1
+					while @var2 != @var
+						begin
+							fetch from RedoCursor into @statements
+							select @var2 = @var2+1
+						end
+
+						select @procedure = 'exec ' + @statements
+						print @procedure
+						print 'this was the procedure'
+						exec sp_executesql @procedure
+						update VersionHistory
+							set version = version + 1
+						set @var = @var + 1
+						--fetch from RedoCursor into @statements
+
+					close RedoCursor
+					deallocate RedoCursor
+					end
+end
+go
+
+
+insert into ProcedureTable(UndoProcedure, RedoProcedure, Version)
+values
+		('DropTableCarOwners', 'CreateTableCarOwners', 1),
+		('RemoveFamilyName', 'AddFamilyName', 2),
+		('SetRepairCostBack', 'SetRepairCost', 3),
+		('RemoveDefault', 'DefaultWorkshopSpace15', 4),
+		('RemoveCarOwnersCK', 'AddCarOwnersCK', 5),
+		('RemoveCarOwnersIdFK', 'AddCarOwnersIdFK', 6),
+		('RemoveCarOwnersPK', 'AddCarOwnersPK', 7)
+
+select * from ProcedureTable
+select * from VersionHistory
+
+execute GoToVersion @Version = 0
+
+
